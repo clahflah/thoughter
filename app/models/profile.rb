c966aa315382fca535abf8852b961d8a44dba006
clahflah <claytonflaherty92@gmail.com>
2023-01-03 18:48:55 +0100

    profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :user_name, :first_name, :last_name, :bio, :location, presence: true
+  validates :user_name, uniqueness: true
+  validates :bio, length: { minimum: 10 }
 end
