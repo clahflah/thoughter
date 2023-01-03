241eb828969cacfc7edfa5b26c5724c8f216ab2d
clahflah <claytonflaherty92@gmail.com>
2023-01-03 19:01:02 +0100

    models and validations complete

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :friendships
   validates :user_name, :first_name, :last_name, :bio, :location, presence: true
   validates :user_name, uniqueness: true
   validates :bio, length: { minimum: 10 }
