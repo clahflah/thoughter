ea07e91353754d300da3c7e0669031acb591e0d8
clahflah <claytonflaherty92@gmail.com>
2023-01-03 18:59:59 +0100

    friendship model

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,3 +1,6 @@
 class Thought < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true
+  validates :content, length: { maximum: 180 }
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :profile
+  has_many :thoughts
+  has_many :shared_thoughts
+
 end
